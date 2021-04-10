bc634a5d1a0687255da3b155f0017de316a72646
Martin-Justin-Lang <martin.lang67@gmail.com>
2021-04-10 22:25:17 +0200

    commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -23,4 +23,9 @@ class HomeController extends AbstractController
     {
         return $this->twig->render('Home/index.html.twig');
     }
+
+    public function navbar()
+    {
+        return $this->twig->render('Home/navbar.html.twig');
+    }
 }
